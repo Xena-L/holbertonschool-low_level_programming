6a0505aff58d06ab06d0dcf683bf14bc51b7857a
Xena-L <Apranik@outlook.fr>
2025-10-23 13:36:05 +0200

    project 2 tasks 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main  - Entry point
+*
+* Description: Write a program that prints the alphabet in lowercase,
+* and then in uppercase, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (x = 'A' ; x <= 'Z'; x++)
+{
+putchar(x);
+}
+
+putchar('\n');
+
+return (0);
+}
