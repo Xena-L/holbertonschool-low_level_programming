b008a78c8672a200606aad6a64045a665a815bd4
Xena-L <Apranik@outlook.fr>
2025-10-24 10:12:53 +0200

    project 1 tasks 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,8 @@
 /**
 * main - Entry point
 *
-* Description :Write a program that prints all single digit numbers of base 10 starting from 0,
+* Description :Write a program that prints all single
+* digit numbers of base 10 starting from 0.
 * followed by a new line.
 *
 * Return: Always 0 (Success)
@@ -12,11 +13,11 @@
 int main(void)
 {
 int x;
-for(x = 0; x <= 9; x++)
-{
-putchar(x + '0')
-}
+
+for (x = 0; x < 10; x++)
+putchar(x + '0');
 
 putchar('\n');
 
-return (0)
+return (0);
+}
