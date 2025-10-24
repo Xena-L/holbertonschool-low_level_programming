74f534fdbbaa3f15ad256e750544fcc367a58d5e
Xena-L <Apranik@outlook.fr>
2025-10-24 13:45:58 +0200

    project 1 tasks 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Displays a program all rhe numbers of base 16 in lowercasa,
+* followed by new line.
+* i'am only use putchar three times.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int x;
+int z;
+
+for (x = 0 ; x <= 9; x++)
+putchar(x);
+for (z = 'a' ; z <= 'f'; x++)
+putchar(z);
+
+putchar('\n');
+
+return (0);
+}
