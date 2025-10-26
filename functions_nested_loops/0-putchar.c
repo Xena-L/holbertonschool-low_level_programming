9e33f85de3a52b92a5bbf37bd6e3fd9ed6106d84
Xena-L <Apranik@outlook.fr>
2025-10-26 20:01:09 +0100

    project 4 tasks 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description : Write a program that prints _putchar, followed by new line.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("_putchar");
+
+putchar('\n');
+
+return (0);
+}
