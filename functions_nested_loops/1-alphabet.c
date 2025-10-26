c1348bcf955a52216dc0dde7284cf675e3e49422
Xena-L <Apranik@outlook.fr>
2025-10-26 21:10:39 +0100

    project 4 tasks 0

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description : print alphabet in lowercase
+*
+* Return: Always 0.
+*/
+
+void print_alphabet(void);
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+
+_putchar(c);
+
+_putchar('\n');
+}
+
