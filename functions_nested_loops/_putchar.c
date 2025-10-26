95af9696fadb97dc63456fcf84636ff13aa99903
Xena-L <Apranik@outlook.fr>
2025-10-26 21:19:52 +0100

    project 4 tasks 1

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 
+/**
+ * _putchar - écrit le caractère c sur la sortie standard
+ * @c: caractère à écrire
+ *
+ * Return: 1 si succès, -1 si erreur
+ */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
+
