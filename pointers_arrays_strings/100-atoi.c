1f647c1c977ca68f0f8ea7fa019b34d8a2b3e1da
Xena-L <Apranik@outlook.fr>
2025-11-02 18:04:49 +0100

    project 2 tasks 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,7 @@
 int _atoi(char *s)
 {
 int i = 0, sign = 1, num = 0;
+int digit_found = 0;
 
 while (s[i] != '\0')
 {
@@ -17,8 +18,15 @@ if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
+digit_found = 1;
+if (num > (2147483647 - (s[i] - '0')) / 10)
+{
+return (sign == 1 ? 2147483647 : -2147483648);
+}
 num = num * 10 + (s[i] - '0');
-if (s[i + 1] < '0' || s[i + 1] > '9')
+}
+else if (digit_found)
+{
 break;
 }
 i++;
