3b5c7f7f66fd966438fa9d0419cc74e52c622bd7
Xena-L <Apranik@outlook.fr>
2025-11-02 19:48:49 +0100

    project 3 tasks 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* cap_string - capitalize words
+* @s: string
+* Return: pointer to s
+*/
+
+char *cap_string(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' ||
+s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+s[i] == '{' || s[i] == '}')
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+s[i + 1] -= 32;
+}
+i++;
+}
+
+return (s);
+}
