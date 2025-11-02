e61a458b3e492b3b1474527f2746ff953b5a7bff
Xena-L <Apranik@outlook.fr>
2025-11-02 19:42:35 +0100

    project 3 tasks 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* string_toupper - convert string to uppercase
+* @str: string
+* Return: pointer to str
+*/
+
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 'a' + 'A';
+i++;
+}
+
+return (str);
+}
