3c5ee8979bf023d87052a8ea74976fec41ab4779
Xena-L <Apranik@outlook.fr>
2025-11-07 16:16:50 +0100

    project 2 tasks 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - affiche une chaîne suivie d'un saut de ligne
- * @s: chaîne à afficher
- */
+* _puts_recursion - affiche une chaîne suivie d'un saut de ligne
+* @s: chaîne à afficher
+*/
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* condition de fin */
+	if (*s == '\0')
 	{
-		_putchar('\n'); /* afficher le saut de ligne */
+		_putchar('\n');
 		return;
 	}
 
-	_putchar(*s);       /* afficher le caractère actuel */
-	_puts_recursion(s + 1); /* appeler récursivement pour le reste de la chaîne */
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
