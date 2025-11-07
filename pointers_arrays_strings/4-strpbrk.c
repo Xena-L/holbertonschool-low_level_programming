8c025fde4a37a5a6c42367d5eb3dbedbcad870f1
Xena-L <Apranik@outlook.fr>
2025-11-07 15:38:09 +0100

    project 1 tasks 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,27 +2,26 @@
 #include <stddef.h>
 
 /**
-* _strpbrk - cherche le premier caractère dans s
-*            qui correspond à un caractère de accept
-* @s: la chaîne dans laquelle chercher
-* @accept: la chaîne contenant les caractères à rechercher
-*
-* Return: un pointeur vers le premier caractère correspondant dans s,
-*         ou NULL si aucun caractère n’est trouvé
-*/
-
+ * _strpbrk - cherche le premier caractère dans s
+ * qui correspond à un caractère de accept
+ * @s: la chaîne dans laquelle chercher
+ * @accept: la chaîne contenant les caractères à rechercher
+ *
+ * Return: un pointeur vers le premier caractère correspondant dans s,
+ * ou NULL si aucun caractère n’est trouvé
+ */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i, j;
+	unsigned int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-        {
-if (s[i] == accept[j])
-return (s + i);
-}
-}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
 
-return (NULL);
+	return (NULL);
 }
