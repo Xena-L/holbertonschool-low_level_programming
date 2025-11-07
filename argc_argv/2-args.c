b06d62d4e2f2bb1a899517b6e1f321b25e494f05
Xena-L <Apranik@outlook.fr>
2025-11-07 17:11:02 +0100

    project 3 tasks 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints all arguments passed to the program
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
