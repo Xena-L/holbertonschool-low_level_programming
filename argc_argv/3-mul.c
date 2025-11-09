0c9452780ac2ff707f8531f5f242ad90c9d4450b
Xena-L <Apranik@outlook.fr>
2025-11-09 22:03:16 +0100

    project 3 tasks 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers passed as arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, result;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+
+printf("%d\n", result);
+return (0);
+}
