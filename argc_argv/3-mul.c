af6b7b4b62119fa9e803ad2998862104dc14f123
Xena-L <Apranik@outlook.fr>
2025-11-09 22:05:14 +0100

    project 3 tasks 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two numbers passed as arguments
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success, 1 on error
- */
+* main - multiplies two numbers passed as arguments
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 on success, 1 on error
+*/
 
 int main(int argc, char *argv[])
 {
