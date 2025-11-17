81ec715434f1ef5ae8eb9c793a67c6fe8d98a570
Xena-L <Apranik@outlook.fr>
2025-11-17 15:01:36 +0100

    project 1 tasks 2 // To hell with circumstances; I create opportunities

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,6 +1,14 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+* int_index - searches for an integer in an array using a comparison function
+* @array: the array to search
+* @size: number of elements in the array
+* @cmp: pointer to comparison function
+*
+* Return: index of the first element where cmp != 0, -1 if not found
+*/
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
